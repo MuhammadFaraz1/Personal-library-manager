96b0060eca931c3df86c90114c2a69c68bc39b1a
MuhammadFaraz1 <160864951+MuhammadFaraz1@users.noreply.github.com>
2025-05-02 12:16:39 +0500

    Add files via upload

---
+++ personal_library_manager/main.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import json #load and save library data
+
+
+def load_library():
+    try:
+        with open("library.json","r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+    
+def save_library():
+    with open("library.json","w") as file:
+        json.dump(library,file,indent=4)
+
+#initialize library
+library = load_library()
+
+st.title("Personal Library Manager")
+menu = st.sidebar.radio("Select an option",["View Library","Add Book","Remove Book","Search Book","Save and Exit"])
+if menu == "View Library":
+    st.sidebar.title("Your Library")
+    if library:
+        st.table(library)
+    else:
+        st.write("No book in your library. Add some books!")
+
+#Add book
+elif menu == "Add Book":
+    st.sidebar.title("Add a new book")
+    title = st.text_input("Title")
+    author = st.text_input("Author")
+    year = st.number_input("Year",min_value=2000,max_value=2050,step=1)
+    genre = st.text_input("Genre")
+    read_status = st.checkbox("Mark as read")
+
+    if st.button("Add Book"):
+        library.append({"title":title,"author":author,"year":year,"genre":genre,"read_status":read_status})
+        save_library()
+        st.success("Book added successful")
+        st.rerun()
+
+#remove book
+elif menu == "Remove Book":
+    st.sidebar.title("Remove a book")
+    book_titles = [book["title"] for book in library]
+
+    if book_titles:
+        selected_book = st.selectbox("Select a book to remove",book_titles)
+        if st.button("Remove Book"):
+            library = [book for book in library if book["title"] != selected_book]
+            save_library()
+            st.success("Book removed successfully!")
+            st.rerun()
+        else:
+            st.warning("No book in your library. Add some books!")
+#search book
+elif menu == "Search Book":
+    st.sidebar.title("Search a book")
+    search_term = st.text_input("Enter title or auhtor name")
+
+    if st.button("search"):
+        results = [book for book in library if search_term.lower() in book["title"].lower() or search_term.lower() in book["author"].lower()]
+        if results:
+            st.table(results)
+        else:
+            st.warning("No book found!")
+
+#save and exit
+elif menu == "Save and Exit":
+    save_library()
+    st.success("Library saved successfully!")
